05c7c9071fd0e5a272d5f1fdc21698ad82aff332
Ryan Carins <ryan.carins@riscy.biz>
2019-11-08 23:01:30 +1100

    Change resize to use argparse correctly Bring code up to pep8/argparse standard

--- resize.py
+++ resize.py
@@ -1,22 +1,29 @@
-from PIL import Image
+'''
+Script to resize a directory of images with
+bars on edges instead of stretching
+'''
+
 import glob
 import pathlib
 import argparse
 import sys
+from PIL import Image
 
 
-# Save image as the next available integer
-def saveImage(image):
+def save_image(image, image_type):
+    '''Save image as the next available integer'''
+
     i = 0
-    path = pathlib.Path(f"resized/{i}.png")
-    while(path.exists()):
+    path = pathlib.Path(f"resized/{i}.{image_type}")
+    while path.exists():
         i += 1
-        path = pathlib.Path(f"resized/{i}.png")
-    image.save(f"resized/{i}.png")
+        path = pathlib.Path(f"resized/{i}.{image_type}")
+    image.save(f"resized/{i}.{image_type}")
 
 
-# Create a list of image paths on disk
-def loadImages():
+def load_images():
+    ''' Create a list of image paths on disk '''
+
     # Glob is not case sensitive so this works for PNG, etc
     extensions = ['png', 'jpg', 'jpeg', 'gif', 'tiff', 'bmp']
     paths = list()
@@ -24,16 +31,22 @@ def loadImages():
     # Get the file names of all known image types in input folder
     for extension in extensions:
         paths += glob.glob(f"input/*.{extension}")
-    return(paths)
+    return paths
 
 
-# Create an image of size width x height
-# Add black bars to ensure correct ratio
-def resize(imagePaths, width, height):
+def resize(image_paths, width, height, image_type):
+    '''
+    Create and save image of size width x height
+    Add black bars to ensure correct ratio
+    '''
+
     # Load all paths as Image objects
-    images = [Image.open(i) for i in imagePaths]
+    images = [Image.open(i) for i in image_paths]
     for image in images:
-        canvas = Image.new('RGBA', (width, height), "#000000")
+        if image_type == "png":
+            canvas = Image.new("RGBA", (width, height), "#000000")
+        else:
+            canvas = Image.new("RGB", (width, height), "#000000")
 
         # Find the smallest multiplier so that neither axis is too large
         multiplier = min(height / image.height, width / image.width)
@@ -42,30 +55,30 @@ def resize(imagePaths, width, height):
             int(image.height * multiplier)))
 
         # Find which axis matches the required dimensions
-        if(image.width == width):
+        if image.width == width:
             canvas.paste(image, (0, int((height / 2) - (image.height / 2))))
         else:
             canvas.paste(image, (int((width / 2) - (image.width / 2)), 0))
 
-        saveImage(canvas)
+        save_image(canvas, image_type)
 
 
-# Create folders just to make sure they exist
-pathlib.Path('input').mkdir(parents=True, exist_ok=True)
-pathlib.Path('resized').mkdir(parents=True, exist_ok=True)
+def main():
+    '''Entry point'''
 
-# If there are arguments they are width x height
-# Otherwise assume 1920x1080
-if(len(sys.argv) > 1):
-    parser = argparse.ArgumentParser(description='Image resizer')
-    parser.add_argument('Width', metavar='Width', type=int)
-    parser.add_argument('Height', metavar='Height', type=int)
-    args = parser.parse_args()
-    Width = args.Width
-    Height = args.Width
-else:
-    Width = 1920
-    Height = 1080
+    # Create folders just to make sure they exist
+    pathlib.Path('input').mkdir(parents=True, exist_ok=True)
+    pathlib.Path('resized').mkdir(parents=True, exist_ok=True)
 
-paths = loadImages()
-resize(paths, Width, Height)
+    if len(sys.argv) > 1:
+        parser = argparse.ArgumentParser(description='Image resizer')
+        parser.add_argument('-W', '--width', type=int, default=1920)
+        parser.add_argument('-H', '--height', type=int, default=1080)
+        parser.add_argument('-t', '--type', type=str, choices=["png", "jpg"],
+                            default="jpg")
+        args = parser.parse_args()
+
+    paths = load_images()
+    resize(paths, args.width, args.height, args.type)
+
+main()
