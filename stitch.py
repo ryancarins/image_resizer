6d6ae52b75a8789a3a3551e0836ef69e452153e2
Ryan Carins <ryan.carins@riscy.biz>
2019-11-09 01:56:01 +1100

    Simplify stitch by assuming same resolution

--- stitch.py
+++ stitch.py
@@ -34,12 +34,12 @@ def stitch_images(image_paths, image_type):
     for i, image in enumerate(images):
         # Stitch the previous and current image together
         # Using previous because -1 is not out of bounds
-        size = (images[i-1].width + image.width,
-                max(images[i-1].height, image.height))
+        size = (image.width*2, image.height)
         stitched_image = Image.new(mode, size)
         stitched_image.paste(images[i-1])
-        stitched_image.paste(image, (images[i-1].width, 0))
+        stitched_image.paste(image, (image.width, 0))
         stitched_image.save(f"stitched/{i}.{image_type}")
+
 
 def main():
     '''Entry point'''
